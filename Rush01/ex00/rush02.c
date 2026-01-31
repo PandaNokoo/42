f57acb27bc1cea69a325e68c328a61a12ad90fac
mreboux mreboux <mreboux@f2r11s13.paris.42.school>
2026-01-31 14:52:39 +0100

    q

---
+++ Rush01/ex00/rush02.c
@@ -0,0 +1,49 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   rush02.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mreboux <mreboux@learner.42.tech>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/01/31 12:59:57 by mreboux           #+#    #+#             */
+/*   Updated: 2026/01/31 13:05:36 by mreboux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+void	ft_putchar(char c);
+
+void	ft_print_square(int x, int y, int i, int j)
+{
+	if (i == 1 && j == 1 || i == 1 && j == x)
+		ft_putchar('A');
+	else if (i == y && j == 1 || i == y && j == x)
+		ft_putchar('C');
+	else if (i == 1 || i == y)
+		ft_putchar('B');
+	else if (j == 1 || j == x)
+		ft_putchar('B');
+	else
+		ft_putchar(' ');
+}
+
+void	rush(int x, int y)
+{
+	int	i;
+	int	j;
+
+	if (x > 0 && y > 0)
+	{
+		i = 1;
+		while (i <= y)
+		{
+			j = 1;
+			while (j <= x)
+			{
+				ft_print_square(x, y, i, j);
+				j++;
+			}
+			i++;
+			ft_putchar('\n');
+		}
+	}
+}
