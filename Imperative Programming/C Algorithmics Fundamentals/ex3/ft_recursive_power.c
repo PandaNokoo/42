fb8b9516584d481017dd7197649ab0876c6538ea
mreboux mreboux <mreboux@f2r3s1.paris.42.school>
2026-02-01 15:29:01 +0100

    EWWWW

--- Imperative Programming/C Algorithmics Fundamentals/ex3/ft_recursive_power.c
+++ Imperative Programming/C Algorithmics Fundamentals/ex3/ft_recursive_power.c
@@ -6,7 +6,7 @@
 /*   By: mreboux <mreboux@learner.42.tech>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/31 17:54:31 by mreboux           #+#    #+#             */
-/*   Updated: 2026/01/31 19:40:39 by mreboux          ###   ########.fr       */
+/*   Updated: 2026/02/01 13:59:28 by mreboux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,15 @@
 */
 int	ft_recursive_power(int nb, int power)
 {
-	if (nb == 0 || power == 0)
-		return (1);
 	if (power < 0)
 		return (0);
+	if (power == 0)
+		return (1);
 	return (nb * ft_recursive_power(nb, power - 1));
 }
 /*
 int main(void)
 {
-	printf("%d", ft_recursive_power(5,3));
+	printf("%d", ft_recursive_power(0,0));
 }
 */
