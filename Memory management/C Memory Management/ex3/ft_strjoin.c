6e5cb298fe8dbf66bc53950e16274924b6afecab
mreboux mreboux <mreboux@f1br1s3.paris.42.school>
2026-02-05 16:11:47 +0100

    boucle infini :(

--- Memory management/C Memory Management/ex3/ft_strjoin.c
+++ Memory management/C Memory Management/ex3/ft_strjoin.c
@@ -6,38 +6,89 @@
 /*   By: mreboux <mreboux@learner.42.tech>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/04 19:09:05 by mreboux           #+#    #+#             */
-/*   Updated: 2026/02/04 19:27:55 by mreboux          ###   ########.fr       */
+/*   Updated: 2026/02/05 15:55:03 by mreboux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 
-char	*ft_strjoin(int size, char **strs, char *sep)
+int	ft_strlen(char *str)
 {
 	int	i;
-	char	*join;
 
 	i = 0;
-	join = malloc(sizeof(char) * size);
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(int size, char **strs, char *sep, char *copy)
+{
+	int	i;
+	int	j;
+	int	y;
+
+	i = 0;
+	y = 0;
+	while(i < size)
+	{
+		j = 0;
+		while (strs[i][j]);
+		{
+			copy[y] = strs[i][j];
+			j++;
+			y++;
+		}
+		j = 0;
+		while ((i < size - 1) && sep[j])
+		{
+			copy[y] = sep[j];
+			j++;
+			y++;
+		}
+		i++;
+	}
+	copy[y] = '\0';
+	return (copy);
+}
+
+char	*ft_strjoin(int size, char **strs, char *sep)
+{
+	int		i;
+	char	*join;
+	int		len;
+
+	i = 0;
+	len = 0;
 	if (size == 0)
 	{
-		return ("");
-		free(join);
+		join = malloc(sizeof(char));
+		*join = 0;
+		return (join);
 	}
 	while (i < size)
 	{
-		join[i] = strs[i] + sep;
+		len += ft_strlen(strs[i]);
 		i++;
 	}
+	len += ft_strlen(sep) * (size - 1);
+	join = malloc(sizeof(char) * len + 1);
+	if (!join)
+		return (NULL);
+	join = ft_strcpy(size, strs, sep, join);
+	return (join);
 }
 
 int	main(void)
 {
 	int	size;
-	int	*join;
-	int	**strs = {"WELCOME", "TO", "MY", "WORLD"}
-	int	*sep = {", "}
+	char	*join;
+	char	*strs[] = {"WELCOME", "TO", "MY", "WORLD"};
+	char	*sep = {", "};
 
 	size = 4;
+	join = ft_strjoin(size, strs, sep);
+	#include <stdio.h>
+	printf("%s", join);
+	return (0);
 }
-
