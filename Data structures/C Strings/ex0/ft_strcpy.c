ac82a914a8e677ac477276447b1908e0f28f8624
mreboux mreboux <mreboux@f1r12s11.paris.42.school>
2026-02-11 20:06:27 +0100

    dr monde

---
+++ Data structures/C Strings/ex0/ft_strcpy.c
@@ -0,0 +1,50 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mreboux <mreboux@learner.42.tech>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/11 19:43:59 by mreboux           #+#    #+#             */
+/*   Updated: 2026/02/11 20:05:22 by mreboux          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int	ft_strlen(char *src)
+{
+	int	i;
+
+	i = 0;
+	while (src[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strcpy(char *dest, char *src)
+{
+	int	i;
+	
+	i = 0;
+	while (src[i])
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
+/*
+int	main(void)
+{
+	char	*src = "hello!";
+	char	*dest;
+	dest = malloc(sizeof(char) * (ft_strlen(src) + 1));
+	if (!dest)
+		return (1);
+	printf("%s", ft_strcpy(dest, src));
+	free(dest);
+	return (0);
+}*/
